e79c36317e2ffa9d46c1f1c209191bc55dd1777d
William Gérald Blondel <contact@williamblondel.fr>
2025-07-06 01:12:13 +0200

    Init project

---
+++ VetoPro/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VetoPro.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ VetoPro/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VetoPro.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
